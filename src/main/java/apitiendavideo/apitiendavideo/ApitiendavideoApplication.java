e4e04c097eed98cfee640b12278ff14a4120bee2
Fray León Osorio Rivera <frayosorio@gmail.com>
2023-04-19 19:31:38 -0500

    Agregando filtro de seguridad para los metodos web mediante JWT

--- src/main/java/apitiendavideo/apitiendavideo/ApitiendavideoApplication.java
+++ src/main/java/apitiendavideo/apitiendavideo/ApitiendavideoApplication.java
@@ -2,12 +2,36 @@ package apitiendavideo.apitiendavideo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import apitiendavideo.apitiendavideo.seguridad.FiltroSeguridad;
 
 @SpringBootApplication
 public class ApitiendavideoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApitiendavideoApplication.class, args);
+	}
+
+	@EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable()
+					.addFilterAfter(new FiltroSeguridad(), UsernamePasswordAuthenticationFilter.class)
+					.authorizeRequests()
+					.antMatchers(HttpMethod.GET, "/usuarios/login").permitAll()
+					.anyRequest().authenticated();
+		}
+
 	}
 
 }
