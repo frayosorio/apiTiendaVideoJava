125de63deaf3ff1ec01d1d29aa7efed44023870c
Fray León Osorio Rivera <frayosorio@gmail.com>
2023-05-10 19:22:36 -0500

    Agregando CORS

--- src/main/java/apitiendavideo/apitiendavideo/ApitiendavideoApplication.java
+++ src/main/java/apitiendavideo/apitiendavideo/ApitiendavideoApplication.java
@@ -1,7 +1,10 @@
 package apitiendavideo.apitiendavideo;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -9,6 +12,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import apitiendavideo.apitiendavideo.seguridad.FiltroSeguridad;
 
@@ -25,11 +31,28 @@ public class ApitiendavideoApplication {
 
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
-			http.csrf().disable()
+			http.cors().and().csrf().disable()
 					.addFilterAfter(new FiltroSeguridad(), UsernamePasswordAuthenticationFilter.class)
 					.authorizeRequests()
 					.antMatchers(HttpMethod.GET, "/usuarios/login").permitAll()
 					.anyRequest().authenticated();
+		}
+
+		@Bean
+		public CorsConfigurationSource corsConfigurationSource() {
+			CorsConfiguration configuration = new CorsConfiguration();
+			configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200")); // or add * to allow all origins
+			configuration.setAllowCredentials(true);
+			configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")); // to
+																												// set
+																												// allowed
+																												// http
+																												// methods
+			configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+			configuration.setExposedHeaders(Arrays.asList("custom-header1", "custom-header2"));
+			UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+			source.registerCorsConfiguration("/**", configuration);
+			return source;
 		}
 
 	}
